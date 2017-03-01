b7e1549026b5649f2df3377f307e83d9c8e3a699
JohnLeeNa <s-leejoh@bsd405.org>
2017-02-28 23:19:43 -0800

    Create Hero.java

---
+++ Hero.java
@@ -0,0 +1,32 @@
+public class Hero extends Player{
+    protected String[] inventory;
+    public Hero(String name, int health, int minDamage, int maxDamage, String[] input){
+        super(name, health, minDamage, maxDamage);
+        this.inventory = input;
+    }
+    public void useItem(int choice){
+        if (choice > 0 && choice < 4){
+            String item = inventory[choice-1];
+            if (item.equals("Health potion")){
+                health = health + 20;
+                System.out.println(name + " now has " + health + "health.");
+                inventory[choice-1] = "empty";
+            }
+            else if (item.equals("Damage potion")){
+                maxDamage = maxDamage + 10;
+                System.out.println(name + " can do more damage.");
+                inventory[choice-1] = "empty";
+            }
+            else {
+                System.out.println("That inventory slot is empty!");
+            }
+        }
+        else
+            System.out.println("Inventory slot nonexistent");
+            System.out.println();
+        }    
+    public void show_inventory(){
+        System.out.println("Your inventory holds: 1. " + inventory[0] + "; 2. " + inventory[1] + "; 3. " + inventory[2] + ";");
+    }
+}
+        
