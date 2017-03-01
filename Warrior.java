858b2e00f31e180a788d2b0010e989be081e3281
JohnLeeNa <s-leejoh@bsd405.org>
2017-02-28 23:35:58 -0800

    Create Warrior.java

---
+++ Warrior.java
@@ -0,0 +1,22 @@
+public class Warrior extends Hero{
+    protected double critChance;
+    public Warrior(String name, String[] input){
+        super(name, 100, 20, 10, input);
+        critChance = 0.5;
+    }
+    public void attack(Player enemy){
+        int hitDamage = (int) (Math.random() * (maxDamage+1) + minDamage);
+        if (critChance > Math.random()){
+            hitDamage = hitDamage * 2;
+            System.out.println(name + " gets a critical hit!");
+        }
+        enemy.setHealth(enemy.getHealth() - hitDamage);
+        System.out.println(this.name + " attacks the " + enemy.getName() + " doing " + hitDamage + " damage.");
+        if (enemy.getHealth() <= 0)
+            enemy.setHealth(0);
+        System.out.println(enemy.getName() + " has " + enemy.getHealth() + " health left.");
+        System.out.println();
+    }
+}
+        
+    
